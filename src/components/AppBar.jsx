5bc3b6383cced29296813fc29ded2155a62ffefd
ArminRodriguez1 <44068727+ArminRodriguez1@users.noreply.github.com>
2023-10-27 21:06:31 -0300

    Subida proyecto

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li><a href="/otra-pagina">Otra Página</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from '../components/AppBar.jsx';
+
+const ProductList = () => {
+  return (
+    <div>
+      <AppBar />
+      {/* Contenido de la página de lista de productos */}
+    </div>
+  );
+};
+
+export default ProductList;
